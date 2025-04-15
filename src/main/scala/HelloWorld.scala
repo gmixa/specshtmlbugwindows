1e1bafe23db676307a7776f49e240d38438b86f8
Gerald Mixa <Gerald@mixas.de>
2025-04-15 21:31:22 +0200

    cleanup

--- src/main/scala/HelloWorld.scala
+++
@@ -1,8 +0,0 @@
-import zio.*
-import java.io.IOException
-object HelloWorld extends ZIOAppDefault:
-  def sayHello: ZIO[Any, IOException, Unit] =
-    Console.printLine("Hello, World!")
-  end sayHello
-  def run: ZIO[Any, IOException, Unit] = sayHello
-end HelloWorld
